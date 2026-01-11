170e6e7e936de2288252a4bd350a1eda9463e158
AryanLuv <aryaneos11@gmail.com>
2026-01-11 23:44:52 +0530

    Clean server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,31 @@
 import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express';
+import { redisClient } from './src/config/redisDb.js';
+import { master } from './src/config/db.js';
+
+
+
 const app=express();
 
 
 
+app.use(express.json())
 
-app.listen(process.env.BACKEND_PORT,()=>{
-    console.log(`Server running at port ${process.env.BACKEND_PORT}`)
-})
 
+// app.listen(process.env.BACKEND_PORT,()=>{
+//     console.log(`Server running at port ${process.env.BACKEND_PORT}`)
+// })
+
+const Initialize_db_Connection=async()=>{
+    try{
+        await Promise.all([master(),redisClient.connect()]);
+        console.log('Both database connected succesfully')
+        app.listen(process.env.BACKEND_PORT,()=>{
+            console.log(`Server listening at port: ${process.env.BACKEND_PORT}`)
+        })
+    }catch(err){
+        console.log(`Error occured while connecting to the database ${err}`)
+    }
+}
+Initialize_db_Connection()
